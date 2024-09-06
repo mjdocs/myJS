a0fad7f2b4a765e1e93cfa4a06fed981302043bd
Denis <bronzovdenis59@gmail.com>
2024-09-06 20:49:39 +0300

    MJ-37: add new dictionary

---
+++ src/utils/helpProjects.ts
@@ -0,0 +1,46 @@
+import { ILinkedImage } from '../types/interfaces'
+
+export const HELP_PROJECTS = [
+    {
+        id: 0,
+        url: 'https://tc39.es/',
+        src: '/myJS/img/tc39.png',
+        alt: 'TC39 logo',
+    },
+    {
+        id: 1,
+        url: 'https://v8.dev/',
+        src: '/myJS/img/v8.png',
+        alt: 'V8 logo',
+    },
+    {
+        id: 2,
+        url: 'https://spec.whatwg.org/',
+        src: '/myJS/img/whatwg.png',
+        alt: 'WHATWG logo',
+    },
+    {
+        id: 3,
+        url: 'https://www.wikipedia.org/',
+        src: '/myJS/img/wiki.png',
+        alt: 'Wikipedia logo',
+    },
+    {
+        id: 4,
+        url: 'https://www.youtube.com/@AsForJS',
+        src: '/myJS/img/asforjs.png',
+        alt: 'As for JS logo',
+    },
+    {
+        id: 5,
+        url: 'https://www.chromium.org/Home/',
+        src: '/myJS/img/chromium.png',
+        alt: 'Chromium logo',
+    },
+    {
+        id: 6,
+        url: 'https://github.com/mjdocs',
+        src: '/myJS/img/myJS.png',
+        alt: 'mjdocs logo',
+    },
+] satisfies Array<ILinkedImage>
