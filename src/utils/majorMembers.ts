ec1f4b2da1449f2ab9777c4296eee91a78a72561
Denis <bronzovdenis59@gmail.com>
2024-09-08 18:37:15 +0300

    MJ-38: add member dictionary

---
+++ src/utils/majorMembers.ts
@@ -0,0 +1,10 @@
+import { ILinkedImage } from '../types/interfaces'
+
+export const MAJOR_MEMBERS = [
+    {
+        id: 0,
+        src: '/myJS/img/profile.png',
+        url: 'https://github.com/denlove',
+        alt: 'denlove avatar',
+    },
+] satisfies Array<ILinkedImage>
